6eed5bea2217905bc561534e4311c8daf525cead
Jeongyong Park <1239824@naver.com>
2023-06-20 20:06:32 +0900

    chore #12 : 테이블 추가에 따른 스키마 변경
    - 옵션 타입을 나타내기 위한 `option_type` 테이블 추가에 따른 스키마 변경
    - `option`이 예약어이기 때문에 `options`로 테이블 명 수정

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -1,116 +1,142 @@
 -- MySQL Workbench Forward Engineering
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+SET @OLD_UNIQUE_CHECKS = @@UNIQUE_CHECKS, UNIQUE_CHECKS = 0;
+SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0;
+SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE =
+        'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
 -- Schema kiosk
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `kiosk` DEFAULT CHARACTER SET utf8 ;
-USE `kiosk` ;
+
+-- -----------------------------------------------------
+-- Schema kiosk
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `kiosk` DEFAULT CHARACTER SET utf8;
+USE `kiosk`;
 
 -- -----------------------------------------------------
 -- Table `kiosk`.`category`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `kiosk`.`category` (
-  `id` INT NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `kiosk`.`category`
+(
+    `id`   INT         NOT NULL,
+    `name` VARCHAR(45) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `kiosk`.`item`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `kiosk`.`item` (
-  `id` INT NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
-  `price` INT NOT NULL,
-  `image` VARCHAR(512) NOT NULL,
-  `category_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_item_category_idx` (`category_id` ASC) VISIBLE)
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `kiosk`.`item`
+(
+    `id`          INT          NOT NULL,
+    `name`        VARCHAR(45)  NOT NULL,
+    `price`       INT          NOT NULL,
+    `image`       VARCHAR(512) NOT NULL,
+    `category_id` INT          NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `fk_item_category_idx` (`category_id` ASC) VISIBLE
+) ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `kiosk`.`payment`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `kiosk`.`payment` (
-  `id` INT NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
-  `image` VARCHAR(512) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `kiosk`.`payment`
+(
+    `id`    INT          NOT NULL,
+    `name`  VARCHAR(45)  NOT NULL,
+    `image` VARCHAR(512) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `kiosk`.`orders`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `kiosk`.`orders` (
-  `id` INT NOT NULL,
-  `amount` INT NOT NULL COMMENT '지불금액',
-  `total` INT NOT NULL COMMENT '총 결제금액',
-  `change` INT NOT NULL COMMENT '거스름돈',
-  `order_date` DATETIME NOT NULL,
-  `payment_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_orders_payment1_idx` (`payment_id` ASC) VISIBLE)
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `kiosk`.`orders`
+(
+    `id`         INT      NOT NULL,
+    `amount`     INT      NOT NULL COMMENT '지불금액',
+    `total`      INT      NOT NULL COMMENT '총 결제금액',
+    `change`     INT      NOT NULL COMMENT '거스름돈',
+    `order_date` DATETIME NOT NULL,
+    `payment_id` INT      NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `fk_orders_payment1_idx` (`payment_id` ASC) VISIBLE
+) ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `kiosk`.`order_item`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `kiosk`.`order_item` (
-  `id` INT NOT NULL,
-  `item_quantity` INT NOT NULL,
-  `price` INT NOT NULL,
-  `item_id` INT NOT NULL,
-  `orders_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_order_item_item1_idx` (`item_id` ASC) VISIBLE,
-  INDEX `fk_order_item_orders1_idx` (`orders_id` ASC) VISIBLE)
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `kiosk`.`order_item`
+(
+    `id`            INT NOT NULL,
+    `item_quantity` INT NOT NULL,
+    `price`         INT NOT NULL,
+    `item_id`       INT NOT NULL,
+    `orders_id`     INT NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `fk_order_item_item1_idx` (`item_id` ASC) VISIBLE,
+    INDEX `fk_order_item_orders1_idx` (`orders_id` ASC) VISIBLE
+) ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `kiosk`.`option`
+-- Table `kiosk`.`option_type`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `kiosk`.`option` (
-  `id` INT NOT NULL,
-  `name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `kiosk`.`option_type`
+(
+    `id`   INT         NOT NULL,
+    `name` VARCHAR(45) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `kiosk`.`options`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `kiosk`.`options`
+(
+    `id`             INT         NOT NULL,
+    `name`           VARCHAR(45) NOT NULL,
+    `option_type_id` INT         NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `fk_option_option_type1_idx` (`option_type_id` ASC) VISIBLE
+) ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `kiosk`.`order_item_option`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `kiosk`.`order_item_option` (
-  `id` INT NOT NULL,
-  `option_id` INT NOT NULL,
-  `order_item_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_order_item_option_option1_idx` (`option_id` ASC) VISIBLE,
-  INDEX `fk_order_item_option_order_item1_idx` (`order_item_id` ASC) VISIBLE)
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `kiosk`.`order_item_option`
+(
+    `id`            INT NOT NULL,
+    `options_id`    INT NOT NULL,
+    `order_item_id` INT NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `fk_order_item_option_option1_idx` (`options_id` ASC) VISIBLE,
+    INDEX `fk_order_item_option_order_item1_idx` (`order_item_id` ASC) VISIBLE
+) ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `kiosk`.`item_option`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `kiosk`.`item_option` (
-  `id` INT NOT NULL,
-  `option_id` INT NOT NULL,
-  `item_id` INT NOT NULL,
-  INDEX `fk_option_has_item_option1_idx` (`option_id` ASC) VISIBLE,
-  INDEX `fk_option_has_item_item1_idx` (`item_id` ASC) VISIBLE,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS `kiosk`.`item_option`
+(
+    `id`         INT NOT NULL,
+    `options_id` INT NOT NULL,
+    `item_id`    INT NOT NULL,
+    INDEX `fk_option_has_item_option1_idx` (`options_id` ASC) VISIBLE,
+    INDEX `fk_option_has_item_item1_idx` (`item_id` ASC) VISIBLE,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
 
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+SET SQL_MODE = @OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS = @OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS = @OLD_UNIQUE_CHECKS;
