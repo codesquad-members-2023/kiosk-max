2b67fc5a1b786ad0afe8583e533c5724883e6080
crtEvent <crtevn@gmail.com>
2023-06-22 01:33:30 +0900

    test #11 : ItemController 아이템 상세 항목 조회 테스트 실패 사례 코드 작성

--- backend/src/test/java/kr/codesquad/kiosk/item/controller/ItemControllerTest.java
+++ backend/src/test/java/kr/codesquad/kiosk/item/controller/ItemControllerTest.java
@@ -12,6 +12,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import kr.codesquad.kiosk.exception.BusinessException;
+import kr.codesquad.kiosk.exception.ErrorCode;
 import kr.codesquad.kiosk.fixture.FixtureFactory;
 import kr.codesquad.kiosk.item.controller.dto.response.ItemDetailsResponse;
 import kr.codesquad.kiosk.item.service.ItemService;
@@ -39,7 +41,25 @@ class ItemControllerTest {
 			)
 			.andDo(print())
 			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.id").value(response.id()));
+			.andExpect(jsonPath("$.id").value(response.id()))
+			.andExpect(jsonPath("$.name").exists())
+			.andExpect(jsonPath("$.price").exists())
+			.andExpect(jsonPath("$.description").exists())
+			.andExpect(jsonPath("$.image").exists())
+			.andExpect(jsonPath("$.options").exists());
+	}
+
+	@DisplayName("존재하지 않는 아이템 아이디로 아이템 하나의 세부 항목을 조회한 경우 404 Not Found를 반환한다.")
+	@Test
+	void givenDoesNotExistItemId_whenGetItemDetails_thenResponse404NotFound() throws Exception {
+		int itemId = 99999999;
+		when(itemService.getItemDetails(itemId)).thenThrow(new BusinessException(ErrorCode.ITEM_NOT_FOUND));
+
+		mockMvc.perform(
+			get("/api/categories/1/items/" + itemId)
+			).andDo(print())
+			.andExpect(status().isNotFound())
+			.andExpect(jsonPath("$.message").exists());
 	}
 }
 
