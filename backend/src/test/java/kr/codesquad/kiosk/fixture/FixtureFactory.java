4afef76e3b9d2737476d8f3e45aaac16e2d07f23
crtEvent <crtevn@gmail.com>
2023-06-26 00:15:18 +0900

    feat #33 : 메뉴 상세 항목 반환 API의 반환 값에 option id 추가

--- backend/src/test/java/kr/codesquad/kiosk/fixture/FixtureFactory.java
+++ backend/src/test/java/kr/codesquad/kiosk/fixture/FixtureFactory.java
@@ -1,14 +1,15 @@
 package kr.codesquad.kiosk.fixture;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import kr.codesquad.kiosk.item.controller.dto.response.ItemDetailsResponse;
+import kr.codesquad.kiosk.item.controller.dto.response.OptionsResponse;
 import kr.codesquad.kiosk.item.domain.Item;
 import kr.codesquad.kiosk.item.domain.Options;
 import kr.codesquad.kiosk.payment.controller.response.PaymentResponse;
 import kr.codesquad.kiosk.payment.domain.Payment;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class FixtureFactory {
 
@@ -20,17 +21,24 @@ public class FixtureFactory {
 		return new Item(1, "콜드 브루", 5000, "url", "description", 1);
 	}
 
-	public static Map<String, List<String>> createOptions() {
-		Map<String, List<String>> map = new HashMap<>();
-		map.put("Size", List.of("Small", "Medium", "Large"));
-		map.put("Temperature", List.of("Ice"));
+	public static Map<String, List<OptionsResponse>> createOptions() {
+		Map<String, List<OptionsResponse>> map = new HashMap<>();
+
+		map.put("Size",
+			List.of(
+				new OptionsResponse(1, "Small"),
+				new OptionsResponse(2, "Medium"),
+				new OptionsResponse(3, "Large")
+			)
+		);
+		map.put("Temperature", List.of(new OptionsResponse(5, "Ice")));
 
 		return map;
 	}
 
 	public static Map<String, List<Options>> createOptionsMap() {
 		return Map.of("Size", List.of(new Options(1, "Small"), new Options(2, "Medium"), new Options(3, "Large")),
-			"Temperature", List.of(new Options(4, "Ice")));
+			"Temperature", List.of(new Options(5, "Ice")));
 	}
 
 	public static List<PaymentResponse> createPaymentResponses() {
