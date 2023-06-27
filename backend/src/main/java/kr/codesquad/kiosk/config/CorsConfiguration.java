4a4e2e039f3bd5f6ebcfd569fdcb9143f4515b66
Jeongyong Park <1239824@naver.com>
2023-06-27 16:31:07 +0900

    feat #48 : localhost 에서도 테스트 가능하도록 변경

--- backend/src/main/java/kr/codesquad/kiosk/config/CorsConfiguration.java
+++ backend/src/main/java/kr/codesquad/kiosk/config/CorsConfiguration.java
@@ -10,7 +10,11 @@ public class CorsConfiguration implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-				.allowedOrigins("http://13.124.226.31:80", "http://43.201.168.11:8080")
+				.allowedOrigins("http://13.124.226.31:80",
+						"http://43.201.168.11:8080",
+						"http://localhost:3000",
+						"http://localhost:8080",
+						"http://127.0.0.1:3000")
 				.allowedMethods("GET", "POST", "OPTION", "PUT");
 	}
 }
