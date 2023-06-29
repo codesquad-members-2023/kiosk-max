e0e98aefa4dc4e2e6e577caa8f0b43f3e2397d93
crtEvent <crtevn@gmail.com>
2023-06-29 11:48:28 +0900

    style #53 : record 비어있는 라인 제거

--- backend/src/main/java/kr/codesquad/kiosk/orders/controller/dto/request/OrdersRequest.java
+++ backend/src/main/java/kr/codesquad/kiosk/orders/controller/dto/request/OrdersRequest.java
@@ -4,5 +4,4 @@ public record OrdersRequest(
 	int paymentId,
 	int amount
 ) {
-
 }
--- backend/src/main/java/kr/codesquad/kiosk/orders/controller/dto/response/OrdersIdResponse.java
+++ backend/src/main/java/kr/codesquad/kiosk/orders/controller/dto/response/OrdersIdResponse.java
@@ -3,5 +3,4 @@ package kr.codesquad.kiosk.orders.controller.dto.response;
 public record OrdersIdResponse(
 	int ordersId
 ) {
-
 }
