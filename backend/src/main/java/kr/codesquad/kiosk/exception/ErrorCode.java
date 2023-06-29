ec66fc10510e953d08a5a592db4827ff8560626d
JeongYong Park <66981851+23Yong@users.noreply.github.com>
2023-06-29 10:24:28 +0900

    fix #53 : conflict 수정 중 발생한 컴파일 에러 해결

--- backend/src/main/java/kr/codesquad/kiosk/exception/ErrorCode.java
+++ backend/src/main/java/kr/codesquad/kiosk/exception/ErrorCode.java
@@ -13,7 +13,7 @@ public enum ErrorCode {
 	NETWORK_FAIN_ERROR(503, "네트워크 오류로 인해 결제에 실패하였습니다."),
 	CARD_LIMIT_EXCEEDED_ERROR(503, "카드 한도 초과로 인해 결제에 실패하였습니다."),
 	MAGNETIC_NOT_RECOGNIZED_ERROR(503, "마그네틱 인식 불량으로 인해 결제에 실패하였습니다."),
-	RESPONSE_DELAY_ERROR(503, "응답이 지연되고 있습니다. 나중에 다시 시도해 주세요.");
+	RESPONSE_DELAY_ERROR(503, "응답이 지연되고 있습니다. 나중에 다시 시도해 주세요."),
 	CATEGORY_NOT_FOUND(404, "해당 카테고리를 찾을 수 없습니다.");
 
 	private final int statusCode;
